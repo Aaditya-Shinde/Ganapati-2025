f4740b783c13c3b7935fe8773080aba6b05bf0bd
Aaditya-Shinde <brightsun004@gmail.com>
2025-08-14 22:01:19 -0700

    Add actions and patterns file

---
+++ actions.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+from consants import all_off, DATA_PINS, CLOCK_PINS, LATCH_PIN
+
+states = [GPIO.LOW, GPIO.HIGH]
+
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LATCH_PIN, GPIO.OUT)
+
+    GPIO.setup(DATA_PINS[0], GPIO.OUT)
+    GPIO.setup(CLOCK_PINS[0], GPIO.OUT)
+    GPIO.setup(DATA_PINS[1], GPIO.OUT)
+    GPIO.setup(CLOCK_PINS[1], GPIO.OUT)
+    GPIO.setup(DATA_PINS[2], GPIO.OUT)
+    GPIO.setup(CLOCK_PINS[2], GPIO.OUT)
+
+
+def shift_out(groups):
+    GPIO.output(LATCH_PIN, GPIO.LOW) 
+    group_no = 0
+    for group in groups:
+        board_number = 1
+        for byte in group:
+            print(f"group={group_no} board={len(group) - board_number} byte= {byte:08b}")
+            i = 0
+            for i in range(8):
+                bit = (byte >> (7-i)) & 1
+                GPIO.output(DATA_PINS[group_no], states[bit])
+                GPIO.output(CLOCK_PINS[group_no], GPIO.HIGH)
+                GPIO.output(CLOCK_PINS[group_no], GPIO.LOW)
+            board_number += 1
+        group_no += 1
+    GPIO.output(LATCH_PIN, GPIO.HIGH)
+
+def send_pattern(pattern):
+    shift_out(all_off)
+    shift_out(pattern)
+
+def cleanup():
+    shift_out(all_off)
+    GPIO.cleanup()
+    print("Cleaned")
