dd97ddfb8ff1a5e9baebcf35f74bfe1035ae3c8e
Aaditya-Shinde <brightsun004@gmail.com>
2025-08-16 12:00:21 -0700

    set values for rows 0 and 1

--- FINAL/individual.py
+++ FINAL/individual.py
@@ -1,11 +1,12 @@
 import time
 import actions
 import constants
+from copy import copy
 
 try:
     actions.setup()
     while True:
-        base = constants.all_off
+        base = copy(constants.all_off)
         group, board, bit = map(int, input("Group(0-5) Board(0-9) Bit(0-7): ").split())
         base[group][-(board+1)] |= 1 << bit
         print(f"{base[group][-(board+1)]:08b}")
