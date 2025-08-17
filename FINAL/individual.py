dfce02dcfa75e349670ed80d5ef566ee87bda7b8
Aaditya-Shinde <brightsun004@gmail.com>
2025-08-16 23:25:11 -0700

    fixed for group 0, others not garaunteed yet

--- FINAL/individual.py
+++ FINAL/individual.py
@@ -1,12 +1,16 @@
 import time
 import actions
-from patterns import individual
+import constants
 from copy import copy
 
 try:
     actions.setup()
     while True:
-        actions.send_pattern(individual)
+        base = copy(constants.all_off)
+        group, board, bit = map(int, input("Group(0-5) Board(0-9) Bit(0-7): ").split())
+        base[group][-(board+1)] |= 1 << bit
+        print(f"{base[group][-(board+1)]:08b}")
+        actions.send_pattern(base)
 
 except KeyboardInterrupt:
     pass
