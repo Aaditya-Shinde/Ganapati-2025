7ddac6a8a71b6b404e9d8730de19974f270af261
Aaditya-Shinde <brightsun004@gmail.com>
2025-08-29 21:07:48 -0700

    Sync music

---
+++ music_sync.py
@@ -0,0 +1,186 @@
+import time
+import RPi.GPIO as GPIO
+from consants import *
+
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LATCH_PIN, GPIO.OUT)
+
+GPIO.setup(DATA_PINS[0], GPIO.OUT)
+GPIO.setup(CLOCK_PINS[0], GPIO.OUT)
+GPIO.setup(DATA_PINS[1], GPIO.OUT)
+GPIO.setup(CLOCK_PINS[1], GPIO.OUT)
+GPIO.setup(DATA_PINS[2], GPIO.OUT)
+GPIO.setup(CLOCK_PINS[2], GPIO.OUT)
+GPIO.setup(DATA_PINS[3], GPIO.OUT)
+GPIO.setup(CLOCK_PINS[3], GPIO.OUT)
+GPIO.setup(DATA_PINS[4], GPIO.OUT)
+GPIO.setup(CLOCK_PINS[4], GPIO.OUT)
+GPIO.setup(DATA_PINS[5], GPIO.OUT)
+GPIO.setup(CLOCK_PINS[5], GPIO.OUT)
+
+states = [GPIO.LOW, GPIO.HIGH]
+
+def send_signal(groups):
+    GPIO.output(LATCH_PIN, GPIO.LOW) 
+    group_no = 0
+    for group in groups:
+        board_number = 1
+        for byte in group:
+            print(f"group={group_no} board={len(group) - board_number} byte= {byte:08b}")
+            i = 0
+            for i in range(8):
+                bit = (byte >> (7-i)) & 1
+                GPIO.output(DATA_PINS[group_no], states[bit])
+                GPIO.output(CLOCK_PINS[group_no], GPIO.HIGH)
+                # time.sleep(0.00001)
+                GPIO.output(CLOCK_PINS[group_no], GPIO.LOW)
+            board_number += 1
+        group_no += 1
+    GPIO.output(LATCH_PIN, GPIO.HIGH)
+
+# pygame.mixer.init()
+# pygame.mixer.music.load("/home/pi/Desktop/New/Ganapati/FINAL/1 Ganraya Ganraya Ganraya Ho.mp3")
+# pygame.mixer.music.play()
+
+DELAY = 0.2
+try:
+
+   send_signal(adding_circles_going_out_stage1)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage2)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage3)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage4)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage5)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage6)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage7)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage8)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage9)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage10)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage11)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage12)
+   time.sleep(0.2)
+   send_signal(all_off)
+
+   send_signal(adding_circles_going_out_stage13)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage14)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage15)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(rays_with_5th_circle)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage17)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage18)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage19)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(rays_with_6th_circle)
+   time.sleep(0.2)
+   send_signal(all_off)
+
+   #Repeat
+
+   send_signal(adding_circles_going_out_stage1)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage2)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage3)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage4)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage5)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage6)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage7)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage8)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage9)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage10)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage11)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage12)
+   time.sleep(0.2)
+   send_signal(all_off)
+
+   send_signal(adding_circles_going_out_stage13)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage14)
+   time.sleep(0.25)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage15)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(rays_with_5th_circle)
+   time.sleep(0.25)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage17)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage18)
+   time.sleep(0.25)
+   send_signal(all_off)
+   send_signal(adding_circles_going_out_stage19)
+   time.sleep(0.2)
+   send_signal(all_off)
+   send_signal(rays_with_6th_circle)
+   
+   
+
+   ##########
+   print("pattern")
+   time.sleep(1000)
+except Exception as e:
+    print('*'*50)
+    print(e)
+    print('*'*50)
+finally:
+    send_signal(all_off)
+    GPIO.cleanup()
+    print("Cleaned")
